bc81385a7c91dc9c65c3a2e8a0986b53369a838d
Valentin Bruma <valentine.bvc@gmail.com>
2023-03-13 01:33:21 +0100

    add initial commit & add GUI label, add button, input box and tlist box

---
+++ tasks_list.h
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------
+
+#ifndef tasks_listH
+#define tasks_listH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.Layouts.hpp>
+#include <FMX.ListBox.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *Button1;
+	TLabel *Label1;
+	TEdit *Edit1;
+	TListBox *ListBox1;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
